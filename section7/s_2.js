e7038c1425f9d9659ba4be3c50c65cf2425324d7
tony <chcjswo@gmail.com>
2021-04-09 01:21:00 +0900

    feat: 섹션 7 문제 풀이
    - 버블정렬 문제 풀이

--- section7/s_2.js
+++ section7/s_2.js
@@ -1,7 +1,15 @@
 function solution(n, arr) {
-    let answer = [];
+    let answer = arr;
+
+    for (let i = n - 1; i > 0; i--) {
+        for (let j = i - 1; j >= 0; j--) {
+            if (answer[i] < answer[j]) {
+                [answer[i], answer[j]] = [answer[j], answer[i]];
+            }
+        }
+    }
 
     console.log(answer);
 }
 
-solution(6, [13, 5, 11, 7, 23, 15]);
+solution(8, [13, 5, 11, 7, 23, 15, 1, 10]);
